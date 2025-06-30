58a6ebcf92bfefa2ae8b795be02c16f910b5dc30
vikrant <virat845712@gmail.com>
2025-06-30 14:25:33 +0530

    Project structure ready .venv is done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vikrant Kumar",
+    author_email="vikrantkumar@example.com",
+    packages=find_packages()
+)
